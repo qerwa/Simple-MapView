2a5ef30e4abad18e602587b8197fef089b227c48
D7703_17 <d7703_17@d7703-17ui-imac.local>
2018-06-04 15:25:06 +0900

    change region

--- Simple MapView/ViewController.swift
+++ Simple MapView/ViewController.swift
@@ -16,11 +16,12 @@ class ViewController: UIViewController {
             super.viewDidLoad()
             // Do any additional setup after loading the view, typically from a nib.
             let location = CLLocationCoordinate2D(latitude: 35.166043, longitude: 129.070564)
-            let span = MKCoordinateSpan(latitudeDelta: 0.006, longitudeDelta: 0.006)
-            let region = MKCoordinateRegion(center: location, span: span)
-            
+//            let span = MKCoordinateSpan(latitudeDelta: 0.006, longitudeDelta: 0.006)
+//            let region = MKCoordinateRegion(center: location, span: span)
+//            myMapView.setRegion(region, animated: true)
+
+            let region = MKCoordinateRegionMakeWithDistance(location, 500, 500)
             myMapView.setRegion(region, animated: true)
-            
             //annotation 추가 (핀)
             let loc01 = CLLocationCoordinate2D(latitude: 35.167809, longitude: 129.070555)
             let anno01 = MKPointAnnotation()
@@ -29,11 +30,21 @@ class ViewController: UIViewController {
             anno01.subtitle = "부산광역시 부산진구 양정1동 418-260"
             myMapView.addAnnotation(anno01)
             
+            
             let anno02 = MKPointAnnotation()
             anno02.coordinate = location
             anno02.title = "동의과학대"
             anno02.subtitle = "안녕하세요"
             myMapView.addAnnotation(anno02)
+            
+//            부산광역시 부산진구 양정동 366-1,051-852-0552
+//            35.168092, 129.070367
+            let loc02 = CLLocationCoordinate2D(latitude: 35.168092, longitude: 129.070367)
+            let anno03 = MKPointAnnotation()
+            anno03.coordinate = loc02
+            anno03.title = "토마토도시락"
+            anno03.subtitle = "부산광역시 부산진구 양정동 366-1"
+            myMapView.addAnnotation(anno03)
       }
 
 
